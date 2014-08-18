6e0741f6b8cc18c9dfdeb2e7e9aad93da92419cd
jwarielle <jwarielle@gmail.com>
2014-08-18 09:38:38 -0700

    modified server.js so it can run on port 3000

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ app.use(express.static(__dirname + '/dist')); //makes a static web app
 
 var server = http.createServer(app);
 
-server.listen(process.env.PORT, function() {
-	console.log('server has started on port ' + process.env.PORT);
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), function() {
+	console.log('server has started on port ' + app.get('port'));
 });
 
 //before run on node, have to specify PORT on command line and also, so when you git push and upload to heroku, delete dist from .gitignore file so that you can move it from just your local machine
